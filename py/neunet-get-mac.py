3a7830b90f40ec5cf665204f85fd35488f96b346
hiroshi sendai <hiroshi.sendai@kek.jp>
2012-06-04 09:12:40 +0900

    rename to distinguish the other board.

---
+++ py/neunet-get-mac.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+try:
+    import sitcprbcp
+except ImportError, e:
+    print 'Error: need sitcprbcp python module'
+    sys.exit(e)
+
+def main():
+    ip_address = '192.168.0.16'
+    address    = 0x80
+    length     = 6
+    id         = 100
+
+    data = sitcprbcp.read_registers(ip_address, address, length, id)
+    for i in data:
+        print '%02x' % (ord(i)),
+    print
+    
+if __name__ == '__main__':
+    main()
